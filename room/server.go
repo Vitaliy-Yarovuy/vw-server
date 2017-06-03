acfdfed7f7ff861eaf4d4ec5774d7f2f2b29b098
Vitaliy-Yarovuy <vitaliy.yxz@gmail.com>
2017-06-03 14:53:18 +0300

    start implementing chat functional

---
+++ room/server.go
@@ -0,0 +1,83 @@
+package room
+
+import (
+	"log"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+
+	"net/http"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/Vitaliy-Yarovuy/vw-server/auth"
+	"time"
+)
+
+
+type game struct {
+	author string
+	time time.Time
+}
+
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "pong !!34:2");
+}
+
+
+// serveWs handles websocket requests from the peer.
+func serveWs(hub *Hub, c echo.Context) error {
+
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(*auth.JWTUserClaims)
+	name := claims.Name
+
+
+	conn, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	client := &Client{name: name, hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+	go client.writePump()
+	client.readPump()
+	return nil
+}
+
+
+func Linsten() {
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+
+	hub := newHub()
+	go hub.run()
+
+
+	// CORS userInfo
+	// Allows requests from any `https://labstack.com` or `https://labstack.net` origin
+	// wth GET, PUT, POST or DELETE method.
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
+
+	// Configure middleware with the custom claims type
+	e.Use(middleware.JWTWithConfig(auth.JWT_MIDDLEWARE_CONFIG))
+
+	e.GET("/", hello)
+	e.GET("/ws",func(c echo.Context) error{
+		return serveWs(hub, c)
+	})
+	e.GET("/room", func(c echo.Context) error{
+		return c.JSON(http.StatusOK, echo.Map{
+			"users": hub.getClients(),
+			"games": []game {},
+			"msgs": []string{},
+		})
+	})
+
+	e.Logger.Fatal(e.Start(":3002"))
+}
